94714893736240daf78528e0d0b2abe13d8c7d74
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-04-13 11:50:41 -0300

    tela de signin estrutura basica

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_animmated_app/screens/login/widgets/form_container.dart';
+import 'package:my_animmated_app/screens/login/widgets/sign_up_button.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -15,6 +17,28 @@ class _LoginScreenState extends State<LoginScreen> {
             image: AssetImage("images/background.jpg"),
             fit: BoxFit.cover
           )
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(top:70, bottom: 32),
+                        child: Image.asset("images/tickicon.png",
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.contain,),
+                    ),
+                    FormContainer(),
+                    SignUpButton()
+                  ],
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
