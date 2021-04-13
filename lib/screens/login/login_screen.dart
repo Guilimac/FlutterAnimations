058705cade4857dd0aad21dd9efbcc1cddd15675
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-04-13 12:09:21 -0300

    estrutura para o botão animado

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:my_animmated_app/screens/login/widgets/form_container.dart';
 import 'package:my_animmated_app/screens/login/widgets/sign_up_button.dart';
+import 'package:my_animmated_app/screens/login/widgets/stagger_animation.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin{
+
+  AnimationController _controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2)
+    );
+  }
+
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +44,7 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: EdgeInsets.zero,
           children: [
             Stack(
+              alignment: Alignment.bottomCenter,
               children: [
                 Column(
                   children: [
@@ -35,6 +58,9 @@ class _LoginScreenState extends State<LoginScreen> {
                     FormContainer(),
                     SignUpButton()
                   ],
+                ),
+                StaggerAnimation(
+                  controller: _controller.view
                 )
               ],
             )
